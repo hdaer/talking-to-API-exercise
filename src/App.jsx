82cfbbe129e789627749b58375bf8622cc5d815e
hdaer <hardar333@hotmail.com>
2023-08-21 16:10:42 +0200

    Add files, Updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-install;
-
+import { useState, useEffect } from "react";
 const App = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch("http://localhost:3000/users");
+      const friends = await response.json();
+      setUsers(friends);
+      console.log(friends);
+      console.log(friends[2]);
+      console.log(friends[2].name);
+    };
+    fetchUsers();
+  }, []);
   return (
     <div className="App">
-      <h1>React Hooks exercise starter</h1>
+      <h1>Talking to API exercise: Blogging Site</h1>
+      <ul>
+        {users.map((user) => {
+          return <li key={user.id}>{user.name}</li>;
+        })}
+      </ul>
     </div>
   );
 };
-
 export default App;
